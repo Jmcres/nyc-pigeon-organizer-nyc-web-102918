0e973e9d49a6ffc22f16419cc56651527b17af29
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 19:49:39 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,21 @@
 def nyc_pigeon_organizer(data)
- pigeon_list = {}
- data.each do |category, detail| 
-   detail.each do |name|
-     if pigeon_list.has_key?(name)
-       pigeon_list[name][category] << specific.to_s 
-     else 
-        pigeon_list[name][category] == [specific.to_s]
-end
-else 
-  pigeon_list[name][category] = {}
-  pigeon_list[name][category] == [specific.to_s]
-end 
-end 
-end 
+new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
